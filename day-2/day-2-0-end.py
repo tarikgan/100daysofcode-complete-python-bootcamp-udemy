195286ea26e05e30a5f48e8013acc62d80df25ee
Michael Ranndall Tarigan <tarikgan@gmail.com>
2021-08-22 00:00:04 +0700

    Day 2 unifinished

---
+++ day-2/day-2-0-end.py
@@ -0,0 +1,21 @@
+# Data Type
+# String
+"Hellow"[3]
+"123" + "345"
+#Integer
+123 + 345
+123_345_789
+#Float
+3.14159
+#Boolean
+True
+False
+
+num_char = len(input("What's your name?"))
+print(type(num_char))
+new_num_char = str(num_char)
+print(type(new_num_char))
+print("Your name has " + new_num_char + " characters.")
+
+
+
